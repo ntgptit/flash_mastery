03e583ee7acea8f6981c10c381fbb352ccbcfc03
GiapNT <ntgptit@gmail.com>
2025-12-05 00:58:53 +0900

    feat: Add log4jdbc dependency for SQL logging and create application properties for dev and prod environments

--- flash_mastery_api/src/main/java/com/flash/mastery/entity/Deck.java
+++ flash_mastery_api/src/main/java/com/flash/mastery/entity/Deck.java
@@ -1,6 +1,10 @@
 package com.flash.mastery.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.flash.mastery.constant.ValidationConstants;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -8,10 +12,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.HashSet;
-import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,21 +29,22 @@ import lombok.ToString;
 @AllArgsConstructor
 public class Deck extends BaseAuditEntity {
 
-  @Column(name = "name", nullable = false, length = ValidationConstants.NAME_MAX_LENGTH)
-  private String name;
+    @Column(name = "name", nullable = false, length = ValidationConstants.NAME_MAX_LENGTH)
+    private String name;
 
-  @Column(name = "description")
-  private String description;
+    @Column(name = "description")
+    private String description;
 
-  @Column(name = "card_count", nullable = false)
-  private int cardCount;
+    @Column(name = "card_count", nullable = false)
+    private int cardCount;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "folder_id")
-  @ToString.Exclude
-  private Folder folder;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "folder_id")
+    @ToString.Exclude
+    private Folder folder;
 
-  @OneToMany(mappedBy = "deck", fetch = FetchType.LAZY)
-  @ToString.Exclude
-  private Set<Flashcard> flashcards = new HashSet<>();
+    @OneToMany(mappedBy = "deck", fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @Default
+    private Set<Flashcard> flashcards = new HashSet<>();
 }
