a9652ee79b02cbe56ac8ad6e86859f179f496aca
GiapNT <ntgptit@gmail.com>
2025-12-04 23:43:45 +0900

    feat: Initialize Spring Boot application with basic structure and configuration

---
+++ flash_mastery_api/src/main/java/com/flash/mastery/FlashMasteryApiApplication.java
@@ -0,0 +1,13 @@
+package com.flash.mastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashMasteryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashMasteryApiApplication.class, args);
+	}
+
+}
---
+++ flash_mastery_api/src/test/java/com/flash/mastery/FlashMasteryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flash.mastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashMasteryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
