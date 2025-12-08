872ba01b3e41e5418e71a858a688852e67542329
GiapNT <ntgptit@gmail.com>
2025-12-08 23:26:03 +0900

    refactor: Remove MyBatis configuration from application class and simplify MyBatisConfig
    - Eliminated the MyBatis mapper scan annotation from FlashMasteryApiApplication.
    - Streamlined MyBatisConfig by removing manual SqlSessionFactory configuration, leveraging Spring Boot's auto-configuration instead.
    - Updated comments to clarify the role of MyBatis auto-configuration in the application.

--- flash_mastery_api/src/main/java/com/flash/mastery/FlashMasteryApiApplication.java
+++ flash_mastery_api/src/main/java/com/flash/mastery/FlashMasteryApiApplication.java
@@ -1,11 +1,9 @@
 package com.flash.mastery;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.flash.mastery.repository")
 public class FlashMasteryApiApplication {
 
     public static void main(String[] args) {
--- flash_mastery_api/src/main/java/com/flash/mastery/config/MyBatisConfig.java
+++ flash_mastery_api/src/main/java/com/flash/mastery/config/MyBatisConfig.java
@@ -1,47 +1,17 @@
 package com.flash.mastery.config;
 
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 /**
  * MyBatis configuration.
- * Ensures sqlSessionFactory is properly configured.
+ * Configures mapper scanning for repository interfaces.
+ * MyBatis auto-configuration from Spring Boot will handle SqlSessionFactory creation.
  */
 @Configuration
 @MapperScan("com.flash.mastery.repository")
 public class MyBatisConfig {
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-        
-        // Set mapper locations
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        sessionFactory.setMapperLocations(resolver.getResources("classpath:mapper/**/*.xml"));
-        
-        // Set type aliases package
-        sessionFactory.setTypeAliasesPackage("com.flash.mastery.entity");
-        
-        // Configure MyBatis settings
-        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
-        configuration.setMapUnderscoreToCamelCase(true);
-        configuration.setDefaultFetchSize(100);
-        configuration.setDefaultStatementTimeout(30);
-        configuration.setLogImpl(org.apache.ibatis.logging.slf4j.Slf4jImpl.class);
-        sessionFactory.setConfiguration(configuration);
-        
-        return sessionFactory.getObject();
-    }
+    // MyBatis Spring Boot Starter will auto-configure SqlSessionFactory
+    // based on application.properties settings
 }
 
